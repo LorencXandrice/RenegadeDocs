8b17deca4a8e7ce263c609cf00abcea342361dec
Xandrice <48589989+Xandrice@users.noreply.github.com>
2024-08-08 20:32:48 -0700

    fix: add some things to the config

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,15 +2,36 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Renegade Roleplay</span>,
+  logo: <span style={{ marginLeft: '.4em', fontWeight: 800 }}>Renegade Roleplay</span>,
   
   chat: {
     link: 'https://discord.gg/renegaderoleplay',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Renegade Guidebook',
+    text: (
+      <span>
+        Copyright ©{' '}{new Date().getFullYear() }{' '}
+        <a href="https://docs.renegaderp.net/" target="_blank">
+          | Renegade Roleplay
+        </a>
+      </span>
+    )
   },
+    useNextSeoProps() {
+      return {
+        titleTemplate: 'Renegade Guidebook',
+      }
+    },
+    banner: {
+      key: '2.0-release',
+      text: (
+        <a href="https://discordapp.com/channels/1083964697532973157/1269940627081003080" target="_blank">
+          🎉 Renegade Roleplay 2.0 is released. Read more →
+        </a>
+      )
+    },
+    
 }
 
 export default config
