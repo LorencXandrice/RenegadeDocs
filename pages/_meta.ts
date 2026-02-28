3ad587e0456f9dfe9262723d5b539d73d29eb02a
athenamora <lizv0012@gmail.com>
2026-02-28 08:59:48 -0800

    Remove jobs section

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -1,7 +1,6 @@
 export default {
   index: "Introduction",
   start: "New Character Who This",
-  jobs: "In Character - Job Manuals",
   icrules: "In Character - Bylaws",
   oocrules: "Out of Character - Rules",
   donation: "Donations",
--- pages/jobs/_meta.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  pd: "§1 Police Department",
-  ems: "§2 Emergency Medical Services"
-}
